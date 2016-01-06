1b6b1ca54dceaa39b4c767d2389d7c3a5672c770
MG <mg@mgs-macbook-air.local>
2016-01-06 08:53:40 -0800

    Initial Commit

---
+++ dreamCatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dreamCatcher
+//
+//  Created by MG on 1/6/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dreamCatcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dreamCatcher
+//
+//  Created by MG on 1/6/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
